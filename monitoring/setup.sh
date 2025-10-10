8c1cb53e9b03c8e17d0dac61d4d6bb40b92d71fd
KiarashMafi <kiarash.mafi.2007@gmail.com>
2025-10-10 12:33:21 +0330

    Setup file updated. Granafa installation commands added.

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -1,7 +1,17 @@
+
+# Installing Prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 helm install prometheus prometheus-community/prometheus
 
 kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext
-kubectl port-forward svc/prometheus-server-ext 9090:80
+
+# Installing Grafana
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm install grafana grafana/grafana
+
+kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-ext
+kubectl port-forward svc/grafana-ext 30305:80
